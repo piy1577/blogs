639519d8c086799a3d5ea4cb7d44933abdda3335
piy1577 <chandra7piyush@gmail.com>
2025-08-03 00:01:19 +0530

    user to auth

--- src/main/java/com/omnify/gayatri_blog/controller/UserController.java
+++ src/main/java/com/omnify/gayatri_blog/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("api/auth")
 public class UserController {
     @Autowired
     UserService us;
