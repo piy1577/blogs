52815faf768cc63b8787c7b0b3aa9268b84e7e8f
piy1577 <chandra7piyush@gmail.com>
2025-08-02 23:55:41 +0530

    cors disabled

--- src/main/java/com/omnify/gayatri_blog/config/SecurityConfig.java
+++ src/main/java/com/omnify/gayatri_blog/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
+
 @Configuration
 public class SecurityConfig {
 
