19684bf8330af084dbc8cd5f887d18769e82240b
piy1577 <chandra7piyush@gmail.com>
2025-08-03 13:44:31 +0530

    update method list

--- src/main/java/com/omnify/gayatri_blog/config/SecurityConfig.java
+++ src/main/java/com/omnify/gayatri_blog/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfig = new org.springframework.web.cors.CorsConfiguration();
                     corsConfig.setAllowedOriginPatterns(List.of("http://localhost:3000/", "http://dummy-bucket-69.s3-website.ap-south-1.amazonaws.com/")); // Allow all origins
-                    corsConfig.setAllowedMethods(List.of("*"));
+                    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     corsConfig.setAllowedHeaders(List.of("*"));
                     corsConfig.setAllowCredentials(true); // Set to true only if using cookies or auth headers with specific origins
                     return corsConfig;
