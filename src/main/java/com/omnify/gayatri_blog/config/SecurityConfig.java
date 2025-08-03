94302f4b6d6be51105bb1f675149e06242765d56
piy1577 <chandra7piyush@gmail.com>
2025-08-03 14:05:11 +0530

    cors removed

--- src/main/java/com/omnify/gayatri_blog/config/SecurityConfig.java
+++ src/main/java/com/omnify/gayatri_blog/config/SecurityConfig.java
@@ -21,14 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(request -> {
-                    var corsConfig = new org.springframework.web.cors.CorsConfiguration();
-                    corsConfig.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://dummy-bucket-69.s3-website.ap-south-1.amazonaws.com")); // Allow all origins
-                    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                    corsConfig.setAllowedHeaders(List.of("*"));
-                    corsConfig.setAllowCredentials(true); // Set to true only if using cookies or auth headers with specific origins
-                    return corsConfig;
-                }))
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
