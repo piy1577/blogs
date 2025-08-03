2b896aee7668d6e2bf8666382f3433b813c78105
piy1577 <chandra7piyush@gmail.com>
2025-08-03 22:07:38 +0530

    user id in blog

--- src/main/java/com/omnify/gayatri_blog/model/Blog.java
+++ src/main/java/com/omnify/gayatri_blog/model/Blog.java
@@ -2,7 +2,6 @@ package com.omnify.gayatri_blog.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -38,7 +37,6 @@ public class Blog {
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    @JsonProperty("userId")
     @JsonIgnoreProperties({"name", "email", "password", "blogs"})
     private User user;
 }
